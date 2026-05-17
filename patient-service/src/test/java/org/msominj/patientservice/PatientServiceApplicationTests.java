ba18917b5f232d2e35efaa0df8e06ec6863799ab
Njabulo Msomi <njabulofmsomi@gmail.com>
2026-05-17 20:20:56 +0200

    Initial patient service setup

---
+++ patient-service/src/test/java/org/msominj/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msominj.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
